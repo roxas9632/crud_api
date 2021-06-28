a8d7d392540ee45175b23664bb11c76eade8bf8c
zack <javck.zack@gmail.com>
2021-06-28 11:26:55 +0800

    加上視圖，有bugs待解

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,68 @@
+<html>
+  <head>
+    <script
+      src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.1/dist/alpine.min.js"
+      defer
+    ></script>
+    <link rel="stylesheet" href="{{ asset('styles.css') }}" />
+  </head>
+
+  <body>
+    <div class="main" x-data="init()">
+      <h4 class="font-xxlarge">用 Alpine.js 來尋找文章</h4>
+      <div class="searchArea">
+        <input
+          class="inputText"
+          type="text"
+          placeholder="請輸入關鍵字"
+          x-model="q"
+        />
+        <button class="bg-default" @click="search()">尋找</button>
+      </div>
+      <div>
+        <template x-for="result in results">
+          <div class="postCard">
+            <div>
+              <img x-bind:src="storage/result.pic" />
+            </div>
+            <div>
+              <div class="postDetailItem">
+                <span style="padding-right: 5px">標題:</span
+                ><span><b x-text="result.title">三國志</b></span>
+              </div>
+              <div class="postDetailItem">
+                <span style="padding-right: 5px">內容:</span
+                ><p x-html="result.content">Hello,World</p>
+              </div>
+            </div>
+          </div>
+        </template>
+      </div>
+    </div>
+    <script>
+      var myHeaders = new Headers();
+        myHeaders.append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9kZW1vLnRlc3RcL2FwaVwvYXV0aFwvbG9naW4iLCJpYXQiOjE2MjQzNTYxODQsImV4cCI6MTYyNDM1OTc4NCwibmJmIjoxNjI0MzU2MTg0LCJqdGkiOiJsVTBNU0NYOUkwd0VDRDV3Iiwic3ViIjoxLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.BE7TM6r9_pl54Csz2HYm6N-1S80-VGU0yPuBrWlLldA");
+
+        var requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
+        };
+      function init() {
+        return {
+          results: [],
+          q: "",
+          search: function () {
+            fetch(
+              "http://taoyuan.test/api/posts/query?s=" +
+                this.q ,requestOptions
+            )
+              .then((response) => response.json())
+              .then((response) => (this.results = response.data.posts))
+              .catch((err) => console.log(err));
+          },
+        };
+      }
+    </script>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::view('posts','posts');
+
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
