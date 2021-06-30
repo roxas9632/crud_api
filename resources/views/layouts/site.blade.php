973bb078f4cfbc9cd0ccfca2d3e5362fe6e9c0d2
zack <javck.zack@gmail.com>
2021-06-30 15:23:33 +0800

    把主選單改用選單管理員來提供資料

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -49,7 +49,7 @@
                         <!-- Main-menu -->
                         <div class="main-menu d-none d-lg-block">
                             <nav>
-
+                                {{ menu('frontend','menus.primary') }}
                             </nav>
                         </div>
                         <!-- Header Right -->
@@ -141,6 +141,7 @@
                 <div class="row align-items-center">
                     <div class="col-xl-7 col-lg-8 col-md-7">
                         <div class="footer-copy-right">
+                            <p>{{ setting('site.license_rule') }}</p>
                             <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
   Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
   <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
--- resources/views/menus/primary.blade.php
+++ resources/views/menus/primary.blade.php
@@ -1,27 +1,17 @@
 <ul id="navigation">
-    <li><a href="{{ url('/') }}">Home</a></li>
-    <li><a href="{{ url('/shop') }}">shop</a></li>
-    <li><a href="about.html">about</a></li>
-    <li class="hot"><a href="#">Latest</a>
-        <ul class="submenu">
-            <li><a href="shop.html"> Product list</a></li>
-            <li><a href="product_details.html"> Product Details</a></li>
-        </ul>
-    </li>
-    <li><a href="blog.html">Blog</a>
-        <ul class="submenu">
-            <li><a href="blog.html">Blog</a></li>
-            <li><a href="blog-details.html">Blog Details</a></li>
-        </ul>
-    </li>
-    <li><a href="#">Pages</a>
-        <ul class="submenu">
-            <li><a href="login.html">Login</a></li>
-            <li><a href="cart.html">Cart</a></li>
-            <li><a href="elements.html">Element</a></li>
-            <li><a href="confirmation.html">Confirmation</a></li>
-            <li><a href="checkout.html">Product Checkout</a></li>
-        </ul>
-    </li>
-    <li><a href="contact.html">Contact</a></li>
+    @foreach ($items as $item)
+        <li><a href="{{ url($item->link()) }}">{{ $item->title }}</a>
+            @php
+                $submenu = $item->children;
+            @endphp
+            @if( $submenu && count($submenu) > 0)
+            <ul class="submenu">
+                @foreach ($submenu as $sub_item)
+                    <li><a href="{{ url($sub_item->link()) }}"> {{ $sub_item->title }}</a></li>
+                @endforeach
+            </ul>
+            @endif
+        </li>
+    @endforeach
 </ul>
+
