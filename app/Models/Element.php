d80b6c0de5937e72f3b874cc5b1baf34bfe446c3
zack <javck.zack@gmail.com>
2021-06-30 15:50:40 +0800

    1.將首頁Slider串接後台 2.提供資料

--- app/Models/Element.php
+++ app/Models/Element.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Element extends Model
 {
     use HasFactory;
+
+    public function scopeEnabled($query)
+    {
+        return $query->where('enabled',true)->orderBy('sort','asc');
+    }
 }
